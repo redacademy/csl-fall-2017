2240f4fe87e995843cbb0c2332e60b4a51544757
Jenny <jenniferonsamer@hotmail.com>
2017-12-05 15:51:08 -0800

    Updated Header with Logo & deleted site info.

--- themes/csl/header.php
+++ themes/csl/header.php
@@ -23,8 +23,13 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+				
+				<div class= "site-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo get_template_directory_uri() . '/images/CultureSavesLives_separate.svg'; ?>" class="logo" alt="csl logo" />
+					</a>
+				</div>
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
