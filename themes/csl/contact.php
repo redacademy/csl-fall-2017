e125f0ed73b0924f6f97ed1c9c142326eecd33d5
Kenny <ktran604@hotmail.com>
2017-12-07 15:38:34 -0800

    styled contact page for mobile, made footer more responsive

--- themes/csl/contact.php
+++ themes/csl/contact.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * 
- *Template Name: Contact
+ * Template Name: Contact Page
+ *
  * @package RED_Starter_Theme
  */
 
@@ -9,37 +9,38 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-    
-			<?php while ( have_posts() ) : the_post(); ?>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <header class="entry-header">
-            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-          </header><!-- .entry-header -->
-        
-          <div class="entry-content">
+    <div class="entry-content">
           <div class="contact-page-contact-info">
 						<p><?php echo get_theme_mod( 'title_value' ); ?></p>		
 						<p><?php echo get_theme_mod( 'address_value' ); ?></p>
 						<p><?php echo get_theme_mod( 'province_value' ); ?></p>
-						<div class="weekday-hours">
+						<div class="contact-weekday-hours">
 							<p><?php echo get_theme_mod( 'weekdays_value' ); ?></p>
 							<p><?php echo get_theme_mod( 'weekday_hours_value' ); ?></p>
 						</div>	
-						<div class="weekend-hours">
+						<div class="contact-weekend-hours">
 						<p><?php echo get_theme_mod( 'weekend_value' ); ?></p>
 							<p><?php echo get_theme_mod( 'weekend_hours_value' ); ?></p>
-						</div>
-            <?php echo get_theme_mod( 'content_value' );?>
+            </div>
+            <div class="contact-additional-info">
+              <?php echo get_theme_mod( 'content_value' );?>
+            </div>  
 					</div>
-           
+			<?php while ( have_posts() ) : the_post(); ?>
 
-          </div><!-- .entry-content -->
-        </article><!-- #post-## -->
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <div class="social-info">
+          <p>Stay Connected</p>
+          <a><?php echo '<i class="fa fa-facebook-square" aria-hidden="true"></i>' ?>facebook</a>
+          <a><?php echo '<i class="fa fa-twitter" aria-hidden="true"></i>' ?>twitter</a>
+          <img src="<?php echo get_template_directory_uri() . '/images/eagle1.svg'; ?>" class="eagle-logo" alt="eagle logo" />
+        </div>
+
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
-
