c062168ef67c7752885a38f89a51f2c578ddfd45
Kenny <ktran604@hotmail.com>
2017-12-08 09:53:38 -0800

    made contact page more responsive

--- themes/csl/contact.php
+++ themes/csl/contact.php
@@ -12,9 +12,14 @@ get_header(); ?>
 
     <div class="entry-content">
           <div class="contact-page-contact-info">
-						<p><?php echo get_theme_mod( 'title_value' ); ?></p>		
-						<p><?php echo get_theme_mod( 'address_value' ); ?></p>
-						<p><?php echo get_theme_mod( 'province_value' ); ?></p>
+						<p><?php echo get_theme_mod( 'title_value' ); ?></p>
+						<div class="contact-additional-info">
+              <?php echo get_theme_mod( 'content_value' );?>
+            </div>  
+						<div class="address-province">		
+							<p><?php echo get_theme_mod( 'address_value' ); ?></p>
+							<p><?php echo get_theme_mod( 'province_value' ); ?></p>
+						</div>	
 						<div class="contact-weekday-hours">
 							<p><?php echo get_theme_mod( 'weekdays_value' ); ?></p>
 							<p><?php echo get_theme_mod( 'weekday_hours_value' ); ?></p>
@@ -23,9 +28,6 @@ get_header(); ?>
 						<p><?php echo get_theme_mod( 'weekend_value' ); ?></p>
 							<p><?php echo get_theme_mod( 'weekend_hours_value' ); ?></p>
             </div>
-            <div class="contact-additional-info">
-              <?php echo get_theme_mod( 'content_value' );?>
-            </div>  
 					</div>
 			<?php while ( have_posts() ) : the_post(); ?>
 
