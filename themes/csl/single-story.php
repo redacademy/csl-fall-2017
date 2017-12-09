6622549de1a6559bff6a65c4ea16d882d20a1816
Aaron <aa.griff9@gmail.com>
2017-12-09 13:35:26 -0800

    set up classes on  one-house and origin to style, and cleaned code

--- themes/csl/single-story.php
+++ themes/csl/single-story.php
@@ -1,29 +1,28 @@
 <?php
-
-get_header(); ?>
+/**
+ * Template for displaying Story single-posts.
+ *
+ * @package CSL_Theme
+ */
+ 
+ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-		<?php if ( is_single( $story = 'one-house' ) ) : ?>
+			<?php if ( is_single( $story = 'one-house' ) ) : ?>
 			
-			<?php get_template_part( 'template-parts/content', 'single-story-one-house' ); ?>
+				<?php get_template_part( 'template-parts/content', 'single-story-one-house' ); ?>
 
-		<?php elseif ( is_single( $story = 'origin' ) ) : ?>
-			<?php get_template_part( 'template-parts/content', 'single-story-origin' ); ?>
+			<?php elseif ( is_single( $story = 'origin' ) ) : ?>
+				<?php get_template_part( 'template-parts/content', 'single-story-origin' ); ?>
 
-		<?php elseif ( is_single( $story = 'vision' ) ) : ?>
-			<?php get_template_part( 'template-parts/content', 'single-story-vision' ); ?>
+			<?php elseif ( is_single( $story = 'vision' ) ) : ?>
+				<?php get_template_part( 'template-parts/content', 'single-story-vision' ); ?>
 
-		<?php elseif ( is_single( $story = 'heritage' ) ) : ?>
-			<?php get_template_part( 'template-parts/content', 'single-story-heritage' ); ?>
+			<?php elseif ( is_single( $story = 'heritage' ) ) : ?>
+				<?php get_template_part( 'template-parts/content', 'single-story-heritage' ); ?>
 
-<?php endif; ?>
-
-			<?php the_post_navigation(); ?>
-
-
+			<?php endif; ?>
 
 		<?php endwhile; // End of the loop. ?>
 
-<footer class="entry-footer">
-	<?php get_footer(); ?>
-</footer><!-- .entry-footer -->
+<?php get_footer(); ?>
--- themes/csl/template-parts/content-single-story-one-house.php
+++ themes/csl/template-parts/content-single-story-one-house.php
@@ -1,71 +1,66 @@
 <?php
 /**
- * Template part for displaying single posts.
+ * Template part for displaying Story - One House post.
  *
- * @package QOD_Starter_Theme
+ * @package CSL_Theme
  */
-
-
-
-//  $team_member_name = get_post_meta( get_the_ID(), '_story_team_member_group_name', true );
-
-//  $team_member_title = get_post_meta( get_the_ID(), '_story_title', true );
-
-//  $team_member_description = get_post_meta( get_the_ID(), '_story_description', true );
-
  ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
 
+  <?php 
+    $alt_title = get_post_meta( get_the_ID(), '_story_alt_title', true ); 
+    $team_member_group = get_post_meta( get_the_ID(), '_story_team_member_group', true ); 
+  ?>
+
+  <header class="entry-header">
+    
+    <h1 class="story__one-house-title">
+      <?php the_title() ?>
+      <span class="story__one-house-alt-title"><?php echo $alt_title ?></span> 
+    </h1>
+
+    <?php if ( has_post_thumbnail() ) : ?>
+			<div class="story__one-house-hero">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+    <?php endif; ?>
+    
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
-
   <?php if ( get_post_meta(get_the_ID(), '_story_team_member_group', true ) ) : ?>
-  <div class="meta-data">
-
-<?php 
-
-// $array_length = count( $team_member_group );
-// d($array_length);
-// d( $team_member_group[0]['_story_name'] ); 
-
-  $team_member_group = get_post_meta( get_the_ID(), '_story_team_member_group', true );
   
-?>
+  <section class="meet-our-team">
 
+    <?php ?>
 
-    <h2>Meet our Team</h2>
-<?php
-  foreach( $team_member_group as $team_member ){
-    if(isset($team_member['_story_name'])){ ?>
-  <?php  $team_member_image = wp_get_attachment_image( $team_member['_story_member_image_id'], 'medium' ); ?>
-
-    <div class="team-member-image">
-      <?php echo $team_member_image; ?>
+    <div class="our-team-title">
+      <h2>Meet our Team</h2>
     </div>
-    <h3><?php echo ( $team_member['_story_name'] ); ?></h3>
-    <p><?php echo ( $team_member['_story_title'] ); ?></p>
-    <p><?php echo ( $team_member['_story_member_description'] ); ?></p>
-
-
 
     <?php
-    }
-  }
+      foreach( $team_member_group as $team_member ){
+        if(isset($team_member['_story_name'])){ ?>
+      <?php  $team_member_image = wp_get_attachment_image( $team_member['_story_member_image_id'], 'medium' ); ?>
+        <section class="team-member">
+          <div class="team-member__image">
+            <?php echo $team_member_image; ?>
+          </div>
+          <h3 class="team-member__name" ><?php echo ( $team_member['_story_name'] ); ?></h3>
 
-?>
-			<?php endif; ?>
+          <h3 class="team-member__title"><?php echo ( $team_member['_story_title'] ); ?></h3>
+
+          <p class="team_member__description"><?php echo ( $team_member['_story_member_description'] ); ?></p>
+        </section>
+    <?php
+        }
+      }
+    ?>
+		<?php endif; ?>
   
-  
-
-
-  </div>
+  </section>
 </article><!-- #post-## -->
