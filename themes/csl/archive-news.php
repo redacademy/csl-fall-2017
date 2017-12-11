530aa25cb3b4c05dcfa1d6cb70001339147a73b2
Ian <ianhitchcock@outlook.com>
2017-12-11 10:32:50 -0800

    fix merge conflict

--- themes/csl/archive-news.php
+++ themes/csl/archive-news.php
@@ -3,7 +3,6 @@
 get_header('desktop');
 get_header(); ?>
 
-<<<<<<< HEAD
   <h2>In The News</h2>
   
 <?php	$news_query = new WP_Query( array( 
@@ -26,23 +25,5 @@ get_header(); ?>
 
 <?php endwhile; ?>
 <?php endif; ?>
-=======
-		<div id="primary">
-			<div id="content" role="main">
-			<h2>In the News</h2>
-			<?php if(have_posts()): ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'template-parts/content', 'news' ); ?>
-
-
-				<?php endwhile; // End of the loop. ?>
-			<?php else : ?>
-						
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-		
-			<?php endif; ?>
-			<a class="twitter-timeline" href="https://twitter.com/cultrsaveslives?ref_src=twsrc%5Etfw">Tweets by cultrsaveslives</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
->>>>>>> 1b3a012732abaed0964a2b3fcedafeed137b9de8
 
 <?php get_footer(); ?>
