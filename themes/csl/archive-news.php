b0e175c02461372fda60e9a528453139e70431b3
Jenny <jenniferonsamer@hotmail.com>
2017-12-10 22:55:48 -0800

    updated front page, archive news, and fixed scss for front page.

--- themes/csl/archive-news.php
+++ themes/csl/archive-news.php
@@ -3,26 +3,28 @@
 get_header('desktop');
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
-      <?php
-      $args = array( 'numberposts' => '8', 'order' => 'DESC');
-			$news_posts = get_posts( $args );
-			$thumbnail = array( 'large' ); ?>
-      
-				<?php while ( have_posts() ) : the_post(); ?>
+  <h2>In The News</h2>
+  
+<?php	$news_query = new WP_Query( array( 
+  'post_type' => 'news', 
+  'posts_per_page' => 8 
+));?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+<?php if ($news_query -> have_posts()):  while ( $news_query->have_posts() ) : $news_query -> the_post(); ?>
+    
+  <?php $news_url = get_post_meta( get_the_ID(), '_article_url', true); ?>
 
-					<?php
-					$news  = get_post_meta( get_the_ID(), '_event_taxonomy_radio', true );
-          echo ( $news );
+  <h3><?php the_title(); ?></h3>
+	
+	<div class="news-archive-image-wrapper">
+	  <a href="<?php echo $news_url ?>">
+		  <?php $news_article_image = wp_get_attachment_image( get_post_meta( get_the_ID(), '_article_image_id', 1 ), 'medium' );
+      echo $news_article_image;
+      ?></a>
+	</div>
 
-					?>
+<?php endwhile; ?>
+<?php endif; ?>
 
-				<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
