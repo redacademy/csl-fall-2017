d4ab49af4b2fa628017ca5c5a63e89c9c7e70559
Ian <ianhitchcock@outlook.com>
2017-12-11 10:53:25 -0800

    Undo merge.

--- themes/csl/archive-news.php
+++ themes/csl/archive-news.php
@@ -3,27 +3,21 @@
 get_header('desktop');
 get_header(); ?>
 
-  <h2>In The News</h2>
-  
-<?php	$news_query = new WP_Query( array( 
-  'post_type' => 'news', 
-  'posts_per_page' => 8 
-));?>
+		<div id="primary">
+			<div id="content" role="main">
+			<h2>In the News</h2>
+			<?php if(have_posts()): ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-<?php if ($news_query -> have_posts()):  while ( $news_query->have_posts() ) : $news_query -> the_post(); ?>
-    
-  <?php $news_url = get_post_meta( get_the_ID(), '_article_url', true); ?>
+					<?php get_template_part( 'template-parts/content', 'news' ); ?>
 
-  <h3><?php the_title(); ?></h3>
-	
-	<div class="news-archive-image-wrapper">
-	  <a href="<?php echo $news_url ?>">
-		  <?php $news_article_image = wp_get_attachment_image( get_post_meta( get_the_ID(), '_article_image_id', 1 ), 'medium' );
-      echo $news_article_image;
-      ?></a>
-	</div>
 
-<?php endwhile; ?>
-<?php endif; ?>
+				<?php endwhile; // End of the loop. ?>
+			<?php else : ?>
+						
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+		
+			<?php endif; ?>
+			<a class="twitter-timeline" href="https://twitter.com/cultrsaveslives?ref_src=twsrc%5Etfw">Tweets by cultrsaveslives</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
 
 <?php get_footer(); ?>
