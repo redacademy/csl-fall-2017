881938e81975bdc066617c8ad792e63cc5e528ff
Kenny <ktran604@hotmail.com>
2017-12-11 16:57:59 -0800

    added animation to mobile menu

--- themes/csl/js/mobile-menu.js
+++ themes/csl/js/mobile-menu.js
@@ -2,7 +2,9 @@
   
   $( '.mobile-menu' ).hide();
   $( '.toggle-open' ).click(function() {
-
+    $('.line1').addClass('.line1-active');
+    $('.line2').addClass('.line2-active');
+    $('.line3').addClass('.line3-active');
     $('#masthead').toggleClass('header-teal');
     $( '.mobile-menu' ).slideToggle( 'slow', function() {
     });
