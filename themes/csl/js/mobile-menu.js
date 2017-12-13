d6429cf0ee35ce86dbeb2768319147c063c770f9
Kenny <ktran604@hotmail.com>
2017-12-13 11:42:38 -0800

    fixed janky mobile menu, added animation to mobile menu button

--- themes/csl/js/mobile-menu.js
+++ themes/csl/js/mobile-menu.js
@@ -1,10 +1,10 @@
 (function ($){
   
-  $( '.mobile-menu' ).hide();
   $( '.toggle-open' ).click(function() {
-    $('.line1').toggleClass('.line1-active');
-    $('.line2').toggleClass('.line2-active');
-    $('.line3').toggleClass('.line3-active');
+    $( '.mobile-menu' ).toggleClass('display');
+    $('.line1').toggleClass('line1-active');
+    $('.line2').toggleClass('line2-active');
+    $('.line3').toggleClass('line3-active');
     $('#masthead').toggleClass('header-teal');
     $( '.mobile-menu' ).slideToggle( 'slow', function() {
     });
