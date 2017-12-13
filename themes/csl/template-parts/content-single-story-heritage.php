3e90dd41990ad48b0e6e7f24bd437ef461167bc0
Aaron <aa.griff9@gmail.com>
2017-12-13 12:40:49 -0800

    style Story

--- themes/csl/template-parts/content-single-story-heritage.php
+++ themes/csl/template-parts/content-single-story-heritage.php
@@ -4,33 +4,34 @@
  *
  * @package Culture_Saves_Lives_Theme
  */
-
- ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-
-  <div class="featured-image">
-    <?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'medium' ); ?>
-    <?php endif; ?>
-  </div><!-- .feature-image -->
-
-  <div class="meta-data">
-
-<?php 
-
-  $heritage_secondary_image = wp_get_attachment_image( get_post_meta( get_the_ID(), '_story_heritage_image_id', 1 ), 'medium' );
-
-  echo $heritage_secondary_image;
 ?>
 
-</div>
+  <?php
+    $heritage_secondary_image = wp_get_attachment_image( get_post_meta( get_the_ID(), '_story_heritage_image_id', 1 ), 'large' );
+  ?>
+
+<article id="post-<?php the_ID(); ?>" class="heritage-container">  
+
+  <header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </header><!-- .entry-header -->
+  
+  <section class="heritage-wrapper">
+    
+    <div class="heritage-featured-image heritage-images">
+      <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail( 'large' ); ?>
+      <?php endif; ?>
+    </div><!-- feature-image -->
+
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div><!-- .entry-content -->
+
+
+    <div class="heritage-image-two heritage-images">
+      <?php echo $heritage_secondary_image; ?>
+    </div>
+
+  </section>
 </article>
