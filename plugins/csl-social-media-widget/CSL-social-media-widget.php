866d9d762dbd5d23044f60450e47bf775dda8310
Kenny <ktran604@hotmail.com>
2017-12-14 09:28:28 -0800

    added the custom social media widgett to not be ignored by git ignore

---
+++ plugins/csl-social-media-widget/CSL-social-media-widget.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * RED WordPress Widget Boilerplate
+ *
+ * The RED Widget Boilerplate is an organized, maintainable boilerplate for building widgets using WordPress best practices.
+ *
+ * Lightly forked from the WordPress Widget Boilerplate by @tommcfarlin.
+ *
+ * @package   CSL_Social_Media
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2015 Your Name or Company Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CSL social media
+ * Plugin URI:        @TODO
+ * Description:       A social media widget
+ * Version:           1.0.0
+ * Author:            @TODO
+ * Author URI:        @TODO
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Prevent direct file access
+if ( ! defined ( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+class CSL_social_media extends WP_Widget {
+
+    /**
+     * @TODO - Rename "widget-name" to the name your your widget
+     *
+     * Unique identifier for your widget.
+     *
+     * @since    1.0.0
+     *
+     * @var      string
+     */
+    protected $widget_slug = 'csl-social-media';
+
+	/*--------------------------------------------------*/
+	/* Constructor
+	/*--------------------------------------------------*/
+
+	/**
+	 * Specifies the classname and description, and instantiates the widget.
+	 */
+	public function __construct() {
+
+		// TODO: update description
+		parent::__construct(
+			$this->get_widget_slug(),
+			'Culture Saves Lives Social Media ',
+			array(
+				'classname'  => $this->get_widget_slug().'-class',
+				'description' => 'Easily add or update the social media handles'
+			)
+		);
+
+	} // end constructor
+
+    /**
+     * Return the widget slug.
+     *
+     * @since    1.0.0
+     *
+     * @return    Plugin slug variable.
+     */
+    public function get_widget_slug() {
+        return $this->widget_slug;
+    }
+
+	/*--------------------------------------------------*/
+	/* Widget API Functions
+	/*--------------------------------------------------*/
+
+	/**
+	 * Outputs the content of the widget.
+	 *
+	 * @param array $args     The array of form elements
+	 * @param array $instance The current instance of the widget
+	 */
+	public function widget( $args, $instance ) {
+
+		if ( ! isset ( $args['widget_id'] ) ) {
+         $args['widget_id'] = $this->id;
+      }
+
+		// go on with your widget logic, put everything into a string and …
+
+		extract( $args, EXTR_SKIP );
+
+		$widget_string = $before_widget;
+
+		// Manipulate the widget's values based on their input fields
+		$title = empty( $instance['title'] ) ? '' : apply_filters( 'widget_title', $instance['title'] );
+		$facebook = empty( $instance['facebook'] ) ? '' : apply_filters( 'facebook', $instance['facebook'] );
+		$twitter = empty( $instance['twitter'] ) ? '' : apply_filters( 'twitter', $instance['twitter'] );
+		$instagram = empty( $instance['instagram'] ) ? '' : apply_filters( 'instagram', $instance['instagram'] );
+		// TODO: other fields go here...
+
+		ob_start();
+
+		if ( $title ){
+			$widget_string .= $before_title;
+			$widget_string .= $title;
+			$widget_string .= $after_title;
+		}
+
+		include( plugin_dir_path( __FILE__ ) . 'views/widget.php' );
+		$widget_string .= ob_get_clean();
+		$widget_string .= $after_widget;
+
+		print $widget_string;
+
+	} // end widget
+
+	/**
+	 * Processes the widget's options to be saved.
+	 *
+	 * @param array $new_instance The new instance of values to be generated via the update.
+	 * @param array $old_instance The previous instance of values before the update.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['facebook'] = strip_tags( $new_instance['facebook'] );
+		$instance['twitter'] = strip_tags( $new_instance['twitter'] );
+		$instance['instagram'] = strip_tags( $new_instance['instagram'] );
+		// TODO: Here is where you update the rest of your widget's old values with the new, incoming values
+
+		return $instance;
+
+	} // end widget
+
+	/**
+	 * Generates the administration form for the widget.
+	 *
+	 * @param array $instance The array of keys and values for the widget.
+	 */
+	public function form( $instance ) {
+
+		// TODO: Define default values for your variables, create empty value if no default
+		$instance = wp_parse_args(
+			(array) $instance,
+			array(
+				'title'     => 'My Widget Title',
+				'facebook'  => '',
+				'twitter'   => '',
+				'instagram' => ''
+
+			)
+		);
+
+		$title = strip_tags( $instance['title'] );
+		$facebook = strip_tags( $instance['facebook'] );
+		$twitter = strip_tags( $instance['twitter'] );
+		$instagram = strip_tags( $instance['instagram'] );
+		// TODO: Store the rest of values of the widget in their own variables
+
+		// Display the admin form
+		include( plugin_dir_path( __FILE__ ) . 'views/admin.php' );
+
+	} // end form
+
+} // end class
+
+// TODO: Remember to change 'Widget_Name' to match the class name definition
+add_action( 'widgets_init', function(){
+     register_widget( 'CSL_social_media' );
+});
