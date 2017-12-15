4a8b996796adda18e2441eb3840c0365371e57d0
Kenny <ktran604@hotmail.com>
2017-12-15 13:36:58 -0800

    deleted blur code

--- themes/csl/js/mobile-menu.js
+++ themes/csl/js/mobile-menu.js
@@ -9,13 +9,4 @@
     $( '.mobile-menu' ).slideToggle( 100, function() {
     });
   });
-
-//   $('.toggle-open').on('blur', function(){
-//     $('.line1').toggleClass('line1-active');
-//     $('.line2').toggleClass('line2-active');
-//     $('.line3').toggleClass('line3-active');
-//     $('#masthead').toggleClass('header-teal');
-//     $('.mobile-menu').slideUp(100, function(){
-//     });
-// });
 })(jQuery);
