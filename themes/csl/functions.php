8998a283f51d4023014db74c482041ae2de86ec1
Kenny <ktran604@hotmail.com>
2017-12-19 16:30:51 -0800

    included a function that removes the posts and comments option in the wordpress dashboard menu

--- themes/csl/functions.php
+++ themes/csl/functions.php
@@ -191,3 +191,13 @@ function seasonal_content($limit){
   return $content;
 }
 
+//
+
+function remove_menus(){
+  
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'edit-comments.php' );          //Comments
+
+}
+add_action( 'admin_menu', 'remove_menus' );
+
